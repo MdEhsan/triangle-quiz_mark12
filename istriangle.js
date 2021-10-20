7b65d2c3b8333502b3d4a2bee6e59460b3ef8c20
MdEhsan <mdehsan57@gmail.com>
2021-10-20 22:47:23 +0530

    updated istriangle code

--- istriangle.js
+++ istriangle.js
@@ -5,34 +5,30 @@ const outputDiv = document.querySelector("#output")
 
 
 
-function calculateSumOfAngle(angle1, angle2, angle3) {
-    const sumOfAngle = angle1 + angle2 + angle3;
-    return (sumOfAngle)
+function sumOfAngle(angle1, angle2, angle3) {
+    const sum = angle1 + angle2 + angle3;
+    return (sum)
 }
-
-
 
 
 function isTriangle() {
-
-    const sumOfAngle = calculateSumOfAngle(Number(input[0].value), Number(input[1].value), Number(input[2].value));
-
-
-    if (sumOfAngle === 0 || sumOfAngle < 0) {
-        outputDiv.innerText = "please put in valid angles"
-        outputDiv.style.color = "red"
-    } else if (sumOfAngle === 180) {
-            outputDiv.innerText = "yes this is a triangle";
-        } else {
-            outputDiv.innerText = "no this is not a triangle";
-        }
-
-    
-
- 
-
-
-}
+    if(input[0].value <0 || input[1].value <0 || input[2].value <0 ){
+       outputDiv.innerText = " Value Of the Angle Cannot be Negative "
+    }else{
+    const sum = sumOfAngle(
+      Number(input[0].value),
+      Number(input[1].value),
+      Number(input[2].value)
+    );
+    if (sum === 180) {
+      console.log("Yay, the angles form a triangle!");
+      outputDiv.innerText = "Yay, the angles form a triangle!";
+    } else {
+      console.log("Oh Oh! The angle doesn't form a triangle");
+      outputDiv.innerText = "Oh Oh! The angle doesn't form a triangle";
+    }
+  }
+  }
 
 
 triangleBtn.addEventListener("click", isTriangle)
